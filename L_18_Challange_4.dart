772ea5b3b94f6296ca9ab6b7fbf60e8bc74466a5
endi julian <endi@iglobal.co.id>
2021-10-25 15:34:14 +0700

    challange

---
+++ L_18_Challange_4.dart
@@ -0,0 +1,15 @@
+void main() {
+  // #1
+  int n = 10;
+  for (int i = 0; i < n; i++) {
+    var bintang = '';
+    for (int j = 0; j <= i; j++) {
+      bintang = bintang + '*';
+    }
+    print(bintang);
+  }
+
+  // #2
+  // var m = 5;
+  // for (int i = 0; i < n; i++) {}
+}
